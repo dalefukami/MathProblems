9f3ed0f198b7fd0f16ccb055fbfa6c6c22528750
Dale Fukami <dale.fukami@gmail.com>
2014-09-13 17:03:02 -0600

    Create missing HTML class

--- lib/HTML.php
+++ lib/HTML.php
@@ -1,1 +1,33 @@
-/home/dale/tools/HTML.php
+<?php
+
+class HTML
+{
+    protected function div($contents, $attributes)
+    {
+        return $this->tag("div", $contents, $attributes);
+    }
+
+    protected function span($contents, $attributes)
+    {
+        return $this->tag("span", $contents, $attributes);
+    }
+
+    protected function tag($name, $contents, $attributes)
+    {
+        $result = "<{$name} ";
+        $result .= $this->attributesToString($attributes).">";
+        $result .= $contents;
+        $result .= "</{$name}>";
+
+        return $result;
+    }
+
+    private function attributesToString($attributes)
+    {
+        $result = "";
+        foreach($attributes as $name => $value) {
+            $result .= "{$name}='{$value}' ";
+        }
+        return $result;
+    }
+}
